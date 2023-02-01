6b6ac97637a6d7ad21764c2f1e34e15fa875a877
Michael Ezea <okeyezea@gmail.com>
2023-02-01 16:02:55 +0100

    html and javascript modification

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("happy birthday")
+let comment = document.getElementById("para1")
+let display = document.getElementById("myCard1")
+let mysum = document.getElementById("mysum")
+function startGame(){
+    let card1 = Math.floor(Math.random()*13)+1
+    let card2 = Math.floor(Math.random()*13)+1
+    
+    sumCards =card1 + card2
+    display.textContent ="Sards: "+ card1 + " ," + card2
+    mysum.textContent = "Sum: " + sumCards
+    renderCards()
+}
+
+function renderCards(){
+    if(sumCards<21){
+        comment.textContent = "Want To Play Another Round?"
+    }
+    else if(sumCards ===21){
+        comment.textContent = "Congratulations! you've Won"
+    }
+    else{
+        comment.textContent = "you're out of the game"
+    }
+}
